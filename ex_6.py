41a11f8fa6f4879320b52bef03343f2f072c34d2
motoko2045 <motoko2045@protonmail.com>
2020-05-31 16:44:35 -0500

    exercise_6

---
+++ ex_6.py
@@ -0,0 +1,27 @@
+# exercise 5 : using if with strings
+# write a small program (using if) that assigns the name of 2 trees to 2 variables,
+# called tree1 and tree2
+
+tree1 = 'big tree'
+tree2 = 'little tree'
+
+if tree1 == tree2:
+    print('too many trees!')
+else:
+    print('perfect variety!')
+
+# and 6 : simple condition
+# write a small program that assigns the value 5 to one variable,
+# called x and the value of 7 called y
+# write a check to see if x is greater, lesser or equal to y
+# using if/elif/else
+
+x = 5
+y = 7
+
+if (x == y):
+    print('they\'re equal!')
+elif (x > y):
+    print('x is larger!')
+else:
+    print('y is the larger number!')
