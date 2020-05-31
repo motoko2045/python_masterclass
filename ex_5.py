49b3093b2ca714ae70421ed96c39285c69644070
motoko2045 <motoko2045@protonmail.com>
2020-05-31 15:48:54 -0500

    exercise_5

---
+++ ex_5.py
@@ -0,0 +1,11 @@
+# using slices, add code to create the following slice:
+
+# qpo
+# edcba
+# slice the string to produce the last 8 characters, in reverse.
+
+str = 'abcdefghijklmnopqrstuvwxyz'
+
+print(str[-10:-13:-1])
+print(str[-22:-27:-1])
+print(str[-1:-9:-1])
