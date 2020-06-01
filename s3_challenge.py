c3191f1381c8cd753e8176f03fbd80106b207ddd
motoko2045 <motoko2045@protonmail.com>
2020-06-01 15:26:43 -0500

    lecture challenge

---
+++ s3_challenge.py
@@ -0,0 +1,9 @@
+parrot = "norwegian blue"
+# challenge is to write "we win" but all on their own line
+
+print(parrot[3])
+print(parrot[4])
+print()
+print(parrot[3])
+print(parrot[6])
+print(parrot[8])
