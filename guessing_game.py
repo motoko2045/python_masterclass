095f73ab65a32ce21b432ce5c062fa0e9fb0e94b
motoko2045 <motoko2045@protonmail.com>
2020-06-02 21:06:58 -0500

    s4_guess_game

--- guessing_game.py
+++ guessing_game.py
@@ -3,20 +3,34 @@ answer = 5
 print('please guess number between 1 and 10: ')
 guess = int(input())
 
-# altered for nesting if/else
-if guess < answer:
-    print('please guess higher')
+if guess != answer:
+    if guess < answer:
+        print('please guess higher')
+    else: # guess must be greater than answer
+        print('please guess lower')
     guess = int(input())
     if guess == answer:
-        print('well done, you guess it!')
-    else:
-        print('sorry, you have not guessed correctly')
-elif guess > answer:
-    print('please guess lower')
-    guess = int(input())
-    if guess == answer:
-        print('well done, you guess it!')
+        print('well done, you guessed it!')
     else:
         print('sorry, you have not guessed correctly')
 else:
     print('you got it the first time!')
+
+
+# altered for nesting if/else
+# if guess < answer:
+#     print('please guess higher')
+#     guess = int(input())
+#     if guess == answer:
+#         print('well done, you guess it!')
+#     else:
+#         print('sorry, you have not guessed correctly')
+# elif guess > answer:
+#     print('please guess lower')
+#     guess = int(input())
+#     if guess == answer:
+#         print('well done, you guess it!')
+#     else:
+#         print('sorry, you have not guessed correctly')
+# else:
+#     print('you got it the first time!')
