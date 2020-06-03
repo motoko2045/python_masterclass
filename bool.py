3a3363b03d91c6a9142a0d2e88722ff020581ebc
motoko2045 <motoko2045@protonmail.com>
2020-06-03 14:46:20 -0500

    lecture work

---
+++ bool.py
@@ -0,0 +1,12 @@
+day = 'Friday'
+temperature = 30
+raining = False
+
+if (day == 'Saturday' and temperature > 27) or not raining:
+    print('Go Swimming')
+else:
+    print('Learn Python')
+
+
+
+    
