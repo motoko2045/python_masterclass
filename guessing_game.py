882e29b6de8b00525f2cf26a9819c0fb8e3afa72
motoko2045 <motoko2045@protonmail.com>
2020-06-02 20:59:16 -0500

    added material

--- guessing_game.py
+++ guessing_game.py
@@ -3,9 +3,20 @@ answer = 5
 print('please guess number between 1 and 10: ')
 guess = int(input())
 
+# altered for nesting if/else
 if guess < answer:
     print('please guess higher')
+    guess = int(input())
+    if guess == answer:
+        print('well done, you guess it!')
+    else:
+        print('sorry, you have not guessed correctly')
 elif guess > answer:
     print('please guess lower')
+    guess = int(input())
+    if guess == answer:
+        print('well done, you guess it!')
+    else:
+        print('sorry, you have not guessed correctly')
 else:
     print('you got it the first time!')
