b2a671628905617dd8e9319898f041dd2dceb40a
motoko2045 <motoko2045@protonmail.com>
2020-06-04 10:52:49 -0500

    lecture work

--- forloops2.py
+++ forloops2.py
@@ -1,17 +1,3 @@
-# we're going to iterate over numbers,
-# and append anything that isn't a digit
-# to the separators string.
+# isnumeric wasn't working because it only works on strings that are unicode.
 
-# this lesson did not work. see line 8
-
-number = '9,223;372:036 854,775;807'
-separators = ""
-
-for char in number:
-    if not char.isnumeric(): # AttributeError: 'str' object has no attribute 'isnumeric'
-        separators = separators + char
-
-print(separators)
-
-values = "".join(char if char not in separators else " " for char in number).split()
-print([int(val) for val in values])
+# REVISIT this issue 
