984f51165895c05fd2ff6fe5e3bf537a747dad43
motoko2045 <motoko2045@protonmail.com>
2020-06-03 20:54:43 -0500

    lecture work

--- in_not-in.py
+++ in_not-in.py
@@ -2,10 +2,17 @@
 
 # two use of 'in', here we focus on 'in' as a condition
 # 'in' is used in sequences
-parrot = 'Norwegian Blue'
-letter = input('Enter a character: ')
+# *************************
+# parrot = 'Norwegian Blue'
+# letter = input('Enter a character: ')
+#
+# if letter in parrot:
+#     print('{} is in {}'.format(letter, parrot))
+# else:
+#     print('I don\'t need that letter')
 
-if letter in parrot:
-    print('{} is in {}'.format(letter, parrot))
-else:
-    print('I don\'t need that letter')
+activity = input('what would you like to do today? ')
+
+if 'cinema' not in activity.casefold:
+    print('But I want to go to the cinema')
+# string comparisons are case sensitive so we use casefold.
