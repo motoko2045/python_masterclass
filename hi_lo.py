dde4c89d8954a7a94da62380fa75cfe9c4c7d4aa
motoko2045 <motoko2045@protonmail.com>
2020-06-05 08:03:47 -0500

    lecture work

--- hi_lo.py
+++ hi_lo.py
@@ -5,7 +5,7 @@ print("Please think of a number between {} and {}".format(low, high))
 input("Press ENTER to start")
 
 guesses = 1
-while True:
+while low != high:
     guess = low + (high - low)//2
     highlow = input('my guess is {}. should I guess higher or lower? Enter h or l, or c if my guess was correct'.format(guess)).casefold()
 
@@ -22,3 +22,6 @@ while True:
     #guesses = guesses + 1
     # augmented assignment
     guesses += 1
+else:
+    print('you thought of the number {}'.format(low))
+    print('I got it in { guesses}'.format(guesses))
